f4d5aa9a9c85747377c9a48deecd4741da348811
O-Tempora <valentraven@gmail.com>
2024-10-12 14:57:15 +0300

    test

--- handler.go
+++ handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"strconv"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
@@ -20,6 +21,15 @@ func (c *Client) EventHandler(ctx context.Context) mqtt.MessageHandler {
 			return
 		}
 		log.Printf("It's fine")
-		log.Print(c.pgClient.Ping())
+		fl, err := strconv.ParseFloat(string(m.Payload()), 64)
+		if err != nil {
+			log.Printf("%v - parsed %v (%s as string)", err.Error(), m.Payload(), string(m.Payload()))
+		}
+		c.pgClient.Exec(`
+				insert into gotest (time, data)
+				values (now(), $1)
+			`,
+			fl,
+		)
 	}
 }
